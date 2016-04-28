5169e4ecbf85e5d6ca98eb2177b7757bf8311418
Jonas Schneider <mail@jonasschneider.com>
2016-04-28 15:07:16 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ ext = Extension(
 )
 
 setup(name='pachi-py',
-      version='0.0.17',
+      version='0.0.18',
       description='Python bindings to Pachi',
       url='https://github.com/openai/pachi-py',
       author='OpenAI',
